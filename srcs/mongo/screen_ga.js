516890a34f6e328f86e0ce0569714c238d2c1d4f
gpaul <gpaul@42.fr>
2022-04-27 03:51:39 +0200

    change name from screen_ga schema

--- srcs/mongo/screen_ga.js
+++ srcs/mongo/screen_ga.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
-const ProfilSchema = new mongoose.Schema({
+const ScreenSchema = new mongoose.Schema({
 	ga_tweet_id: String,
 	image_id: String,
 	base64_img: String
 })
 
-module.exports = mongoose.model("screen_ga", ProfilSchema)
+module.exports = mongoose.model("screen_ga", ScreenSchema)
